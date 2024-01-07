9bf889a3e9f91a482278398295f587f4b006de33
SavageSpino <147405826+SavageSpino@users.noreply.github.com>
2024-01-07 15:22:25 +0200

    sys_ShutDown
    socket_file_ops created, sys_ShutDown complete and Port_Map Created

--- tinyos3-master/kernel_socket.c
+++ tinyos3-master/kernel_socket.c
@@ -1,5 +1,15 @@
 
 #include "tinyos.h"
+#include "kernel_socket.h"
+
+file_ops socket_file_ops = {
+	.Open = (void *) socket_open,
+	.Read = socket_read,
+	.Write = socket_write,
+	.Close = socket_close
+};
+
+Socket_CB* Port_Map[MAX_PORT] = {NULL};
 
 
 Fid_t sys_Socket(port_t port)
@@ -27,6 +37,61 @@ int sys_Connect(Fid_t sock, port_t port, timeout_t timeout)
 
 int sys_ShutDown(Fid_t sock, shutdown_mode how)
 {
+	if(sock < 0 || sock > MAX_FILEID)
+	{
+		return -1;
+	}
+
+	FCB* fcb = get_fcb(sock);
+	if(fcb == NULL)
+	{
+		return -1;
+	}
+
+	Socket_CB* socket_cb = (Socket_CB *)fcb->streamobj;
+	if(socket_cb == NULL)
+	{
+		return -1;
+	}
+
+	if(how = SHUTDOWN_READ)
+	{
+		return pipe_reader_close(socket_cb->peer_s.read_pipe);
+	}
+	if(how = SHUTDOWN_WRITE)
+	{
+		return pipe_writer_close(socket_cb->peer_s.write_pipe);
+	}
+	if(how = SHUTDOWN_BOTH)
+	{
+		int res = 0;
+		res = pipe_reader_close(socket_cb->peer_s.read_pipe) + pipe_writer_close(socket_cb->peer_s.write_pipe);
+		if(res == 0)
+		{
+			return 0;
+		}else{
+			return -1;
+		}
+	}
+	return 0;
+}
+
+int socket_open()
+{
 	return -1;
 }
 
+int socket_read()
+{
+	return -1;
+}
+
+int socket_write()
+{
+	return -1;
+}
+
+int socket_close()
+{
+	return -1;
+}
--- tinyos3-master/kernel_socket.h
+++ tinyos3-master/kernel_socket.h
@@ -1,47 +1,56 @@
-#include "kernel_socket.c"
 #include "tinyos.h"
 #include "util.h"
-#include "kernel_sched.h"
 #include "kernel_dev.h"
 #include "kernel_cc.h"
 #include "kernel_proc.h"
 #include "kernel_streams.h"
+#include "kernel_pipe.h"
+#include "bios.h"
 
 
 
 typedef enum{
-SOCKET_LISTENER,
-SOCKET_UNBOUND,
-SOCKET_PEER     
-        }socket_type;
+  SOCKET_LISTENER,
+  SOCKET_UNBOUND,
+  SOCKET_PEER  
+
+}socket_type;
 
 typedef struct listener_socket{
-rlnode queue;
-CondVar req_available;
+  rlnode queue;
+  CondVar req_available;
+
 }l_socket;
 
 typedef struct peer_socket{
-socket_cb* peer;
-pipe_cb* write_pipe;
-pipe_cb* read_pipe;
+  Socket_CB* peer;
+  Pipe_CB* write_pipe;
+  Pipe_CB* read_pipe;
+
 }p_socket;
 
 
 typedef struct unbound_socket{
-rlnode u_socket;
+  rlnode u_socket;
+
 }u_socket;
 
 
 typedef struct socket_control_block {
-uint refcount;
-FCB* fcb; 
-socket_type type;
-port_t port;
-union{
-listener_socket listener_s;
-unbound_socket  unbound_s;
-peer_socket     peer_s;
-     };
+  uint refcount;
+  FCB* fcb; 
+  socket_type type;
+  port_t port;
+  union{
+    l_socket listener_s;
+    u_socket  unbound_s;
+    p_socket     peer_s;
+  };
   
 }Socket_CB;
+
+int socket_open();
+int socket_read();
+int socket_write();
+int socket_close();
 
